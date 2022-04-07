d238c3fd24d48a62275aae43c78985aca7c94a95
Justine <justine.louarn@icloud.com>
2022-04-07 18:00:00 +0200

    graphique1

--- nyfood.py
+++ nyfood.py
@@ -1,5 +1,6 @@
 #importation des packages
 from pymongo import MongoClient
+import matplotlib.pyplot as plt
 
 #importation de la base
 db_uri = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/"
@@ -14,8 +15,21 @@ cursor_agreg = coll.aggregate([
   }
 ])
 # affichage
+
+liste_nb=[]
+liste_qrt=[]
 for agreg in cursor_agreg:
-    print(agreg["nb_restos"], "restaurants dans le quartier", agreg["_id"])
+    liste_nb.append(agreg["nb_restos"])
+    liste_qrt.append(agreg["_id"])
+print(liste_nb)
+print(liste_qrt)
 
 
+# Création du graphe
+plt.figure(figsize = (10, 5))
+plt.bar(liste_qrt, liste_nb, color ='blue', width = 0.4)
+plt.xlabel("Quartier")
+plt.ylabel("Nombre")
+plt.title("Nombre de restaurants par quartier")
+plt.show()
 
