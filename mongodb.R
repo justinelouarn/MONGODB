886cc75607666e97f5d0ca3f82a41d0912515d7e
Heloise <heloise.bouchard29@gmail.com>
2022-04-20 12:37:51 +0200

    premiererequete

--- mongodb.R
+++ mongodb.R
@@ -1,14 +1,31 @@
 #Test
 library(mongolite)
 
-#1.Connectez vous à la base food
-coll <- mongo("NYfood", url = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/food",options = ssl_options(allow_invalid_hostname=TRUE, weak_cert_validation=TRUE))
+#Connection à la base doctolib
+coll <- mongo("dump_Jan2022", url = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/doctolib",options = ssl_options(allow_invalid_hostname=TRUE, weak_cert_validation=TRUE))
 
-#3.Affichez la liste des index de la collection NYfood.
+#Liste des index de la collection
 coll$index()
 
-#4.Affichez la liste des restaurants de Manhattan dont le nom commence par A.
-q = '{"borough": "Manhattan","name":{"$gte":"A", "$lt": "B"}}'
-res <- as.data.frame(coll$find(q))
-View(res)
+#Centres de vaccination situés à moins de 50km de Rennes
 
+#requete mongodb
+#var ref = {"type": "Point", "coordinates": [-1.6777926, 48.117266]}
+#db.dump_Jan2022.find({"location": {$near : {$geometry : ref, 
+#  $maxDistance: 50000}}})
+
+#requete sur r
+req.coord = coll$aggregate(
+  '[
+    { "$project": { 
+        "name": 1, 
+        "lng": { "$arrayElemAt": ["$location.coordinates", 0]}, 
+        "lat": { "$arrayElemAt": ["$location.coordinates", 1]} 
+    }}
+]')
+View(req.coord)
+
+data = as.data.frame(req.coord[((req.coord$lng<=(-1.17127)) & (req.coord$lng>=(-2.04726))) & (req.coord$lat>=47.718)& (req.coord$lat<=48.5495),])
+View(data) #affiche les centres de vaccination situés à moins de 50km de Rennes
+
+#CARTE
