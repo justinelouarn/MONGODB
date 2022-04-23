ae239e32e38c5e3aba65d1547864be0f7904e535
Justine <justine.louarn@icloud.com>
2022-04-23 14:07:50 +0200

    requete

--- Publication.py
+++ Publication.py
@@ -18,9 +18,9 @@ Requete = db.hal_irisa_2021.aggregate([
                         {"$unwind": "$authors"}, # découpage par auteur
                         {"$group": {"_id": {"name": "$authors.name",
                                             "firstname": "$authors.firstname"}, # groupement par nom+prenom de l'auteur
-                                     "nb": {"$sum": 1}} # comptage des lignes
+                                     "nb_publi": {"$sum": 1}} # comptage des lignes
                         },
-                        {"$sort": {"nb": -1}}, # ordre décroissant
+                        {"$sort": {"nb_publi": -1}}, # ordre décroissant
                         {"$limit": 20} # les 20 premieres lignes
                       ])
 
@@ -28,4 +28,6 @@ resu = []
 for i in Requete:
     resu.append(i)
 
-print(resu)
+#print(resu)
+## {'name': 'Lefèvre', 'firstname': 'Sébastien'}, 'nb_publi': 24}
+### Sébastien Lefèvre a 24 publications
