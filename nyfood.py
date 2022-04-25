87916d2de59341f524682aa6e1a3b411d531c83b
Justine <justine.louarn@icloud.com>
2022-04-25 12:08:50 +0200

    pie chart NY food

--- nyfood.py
+++ nyfood.py
@@ -1,6 +1,15 @@
-#importation des packages
+
+
 from pymongo import MongoClient
-import matplotlib.pyplot as plt
+import pandas as pd
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, show, output_file
+from bokeh.transform import dodge
+from bokeh.layouts import column
+from bokeh.models import Div, HoverTool
+from math import pi
+from bokeh.palettes import Category20c
+from bokeh.transform import cumsum
 
 #importation de la base
 db_uri = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/"
@@ -10,34 +19,77 @@ coll = db["NYfood"]
 print(coll)
 
 # Nombre de restau par quartiers 
-cursor_agreg = coll.aggregate([
-  {"$group": {"_id": "$borough",
-              "nb_restos": {"$sum": 1}}
-  }
-])
-
-# affichage
-
-liste_nb=[]
-liste_qrt=[]
-for agreg in cursor_agreg:
-    liste_nb.append(agreg["nb_restos"])
-    liste_qrt.append(agreg["_id"])
-print(liste_nb)
-print(liste_qrt)
-
-
-# Création du graphe
-plt.figure(figsize = (10, 5))
-plt.bar(liste_qrt, liste_nb, color ='blue', width = 0.4)
-plt.xlabel("Quartier")
-plt.ylabel("Nombre")
-plt.title("Nombre de restaurants par quartier")
-plt.show()
-
-print(liste_nb)
-print(liste_qrt)
+Requete = coll.aggregate([
+                        {"$unwind": "$grades"},
+                        {"$group": {"_id": {"note":"$grades.grade"},
+                                  "nb": {"$sum": 1}}}
+                      ])
 
 
 
-#### TEST HELOISE 
+liste_requete=[]
+for i in Requete:
+    liste_requete.append(i)
+print(liste_requete)
+
+
+
+df1 = pd.DataFrame(liste_requete)
+df1 = pd.DataFrame(df1['_id'].values.tolist(), index=df1.index)
+
+
+
+
+df2 = pd.DataFrame(liste_requete)
+
+df = pd.concat([df1, df2], axis=1)
+df.pop('_id')
+
+source = ColumnDataSource(data=df)
+
+
+
+
+
+x = {
+    'A' : int(df.nb[df.note=="A"]),
+    'B' : int(df.nb[df.note=="B"]),
+    'C' : int(df.nb[df.note=="C"]),
+    'P' : int(df.nb[df.note=="P"]),
+    'Z' : int(df.nb[df.note=="Z"]),
+    'Not Yet Graded' : int(df.nb[df.note=="Not Yet Graded"]),
+}
+
+data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'notes'})
+data['angle'] = data['value']/data['value'].sum() * 2*pi
+data['color'] = Category20c[len(x)]
+
+p = figure(height=350, title="Repartition des notes des restaurants de New-York", toolbar_location=None,
+           tools="hover", tooltips="@notes: @value", x_range=(-0.5, 1.0))
+
+p.wedge(x=0, y=1, radius=0.4,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', legend_field='notes', source=data)
+
+p.axis.axis_label = None
+p.axis.visible = False
+p.grid.grid_line_color = None
+
+show(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
