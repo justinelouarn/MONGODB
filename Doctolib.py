fe05a55bd6cb574c318d505542f406effa41a6cd
Justine <justine.louarn@icloud.com>
2022-04-29 13:29:07 +0200

    change

--- Doctolib.py
+++
@@ -1,9 +0,0 @@
-#importation des packages
-from pymongo import MongoClient
-import matplotlib.pyplot as plt
-
-#importation de la base
-db_uri = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/"
-client = MongoClient(db_uri)
-db = client["doctolib"]
-coll = db["dump_Jan2022"]
